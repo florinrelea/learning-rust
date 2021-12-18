fb3aecf7eee78bfb5c9d7b8157d0d0b9bfcf4cec
florinrelea <florinrelea65@gmail.com>
2021-12-18 21:23:58 +0200

    Init

---
+++ src/date_utils/mod.rs
@@ -0,0 +1,3 @@
+pub fn get_date() -> String {
+  return "This is a cool date".to_owned()
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod date_utils;
+
+
+fn main() {
+  let date = date_utils::get_date();
+
+  println!("{}", date);
+}
